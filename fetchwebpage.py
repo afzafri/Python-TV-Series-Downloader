196e8a509690373983ea9b12904a612b1c7d9390
Afif Zafri <zafri_21596@yahoo.com>
2016-12-13 14:22:08 +0800

    added some code comments

--- fetchwebpage.py
+++ fetchwebpage.py
@@ -1,11 +1,11 @@
-import urllib2
+import urllib2 # library to get content of webpage
 
 class FetchWeb():
 
 	def __init__(self,directoryurl):
-		self.directoryurl = directoryurl
+		self.directoryurl = directoryurl # store receive url into variable
 
 	def run(self):
 		openUrl = urllib2.urlopen(self.directoryurl) # open the url
 		getUrl = openUrl.read() # read/get the html
-		return getUrl
+		return getUrl # return the html content
--- getlinks.py
+++ getlinks.py
@@ -1,19 +1,21 @@
-from HTMLParser import HTMLParser
+from HTMLParser import HTMLParser # for parse HTML 
 
+# create new subclass of HTMLParser
 class GetLinks(HTMLParser):
 
 	def __init__(self,directoryurl):
-		HTMLParser.__init__(self)  
-		self.directoryurl = directoryurl
+		HTMLParser.__init__(self)  # inherit HTMLParser class
+		self.directoryurl = directoryurl # store the received url into variable
 		self.links = [] # store the links in a list
 
 	# this will handle what to do when it find the html opening tag <a>, to get links
 	def handle_starttag(self,tag,attrs):
 		if tag == "a": # if found <a> continue
-			for (attribute, value) in  attrs:
+			for (attribute, value) in  attrs: # loop through the tag
 				if attribute == "href": # get href value only
 					fullurl =  self.directoryurl + value # append directory url to the value, to make complete url
 					self.links.append(fullurl) # insert the url into list
 
+	# function to return the list
 	def getDownLinks(self):
 		return self.links
