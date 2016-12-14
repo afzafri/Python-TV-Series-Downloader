96b62a92e904c0f5930fb5cd811cdb7251cb8a15
Afif Zafri <zafri_21596@yahoo.com>
2016-12-14 12:30:42 +0800

    move filter episode links from main to GetLinks classs

--- getlinks.py
+++ getlinks.py
@@ -3,19 +3,41 @@ from HTMLParser import HTMLParser # for parse HTML
 # create new subclass of HTMLParser
 class GetLinks(HTMLParser):
 
-	def __init__(self,directoryurl):
+	def __init__(self, directoryurl, quality, encoder):
 		HTMLParser.__init__(self)  # inherit HTMLParser class
-		self.directoryurl = directoryurl # store the received url into variable
+		# store the received parameters into variable
+		self.directoryurl = directoryurl 
+		self.quality = quality
+		self.encoder = encoder
 		self.links = [] # store the links in a list
 
 	# this will handle what to do when it find the html opening tag <a>, to get links
-	def handle_starttag(self,tag,attrs):
+	def handle_starttag(self, tag, attrs):
 		if tag == "a": # if found <a> continue
 			for (attribute, value) in  attrs: # loop through the tag
 				if attribute == "href": # get href value only
 					fullurl =  self.directoryurl + value # append directory url to the value, to make complete url
 					self.links.append(fullurl) # insert the url into list
 
-	# function to return the list
-	def getDownLinks(self):
-		return self.links
+	# filter list, we only want to get episode that match our Quality and encoder
+	# then return the list
+	def getFilteredDownLinks(self):
+		# create list
+		self.filteredList = []
+		# iterate through the list
+		for i in range(len(self.links)):
+			links = self.links[i]
+
+			# check if filter x265 encoder is set to True
+			if self.encoder == True:
+				# check if quality and encoder is in the links, add to the list
+				if self.quality in links and "x265" in links:
+					self.filteredList.append(links)
+			else: 
+				# if encoder False, only check quality
+				if self.quality in links:
+					self.filteredList.append(links)
+
+		return self.filteredList # return the list
+
+		
--- main.py
+++ main.py
@@ -1,58 +1,37 @@
 import fetchwebpage
 import getlinks
+import os
 
 class SeriesDownloader():
 
 	# the show details, class var
-	URL = "" # website url, only index directory (no webpage) supported
+	URL = "http://dl.uplodin.ir/Serial/" # website url, only index directory (no webpage) supported
 	SERIES = "The Flash" # show name, must be same as the show directory name
-	SEASON = "S03" # show season, also must be same as the directory name
+	SEASON = "S02" # show season, also must be same as the directory name
 	QUALITY = "720p" # video quality, 1080p, 720p, 480p (refer to the file name)
 	X265ENCODER = True # True if want to download the x265 encoded video. note: also refer to the file name if available or not
-	DIRECTORYURL = URL + SERIES + "/" + SEASON + "/" # complete url 
+	DIRECTORYURL = URL + SERIES + "/" + SEASON + "/" # complete directory url 
 	EPISODES_LIST = SERIES.replace(' ','_') + '_' + SEASON + '_Episodes.txt' # text file for saving the episodes list
 	DOWNLOADED_LIST = SERIES.replace(' ','_') + '_' + SEASON + '_Downloaded.txt' # text file for saving the downloaded episodes
 	
 	def run(self):
 		# for fetching the webpage HTML
-		print "\nGetting page...please wait"
+		print "\nFetching download links...please wait\n"
+		print "---------------------------------------\n"
 		getHTML = fetchwebpage.FetchWeb(self.DIRECTORYURL) # create object
 		pageHTML = getHTML.run()
 
 		# for extracting (parse) the DOWNLOAD LINKS from the webpage HTML
-		print "\nGetting download links...please wait"
-		getLinks = getlinks.GetLinks(self.DIRECTORYURL) # create object
+		getLinks = getlinks.GetLinks(self.DIRECTORYURL, self.QUALITY, self.X265ENCODER) # create object
 		getLinks.feed(pageHTML)
-		linksList = getLinks.getDownLinks()
-		filteredList = self.filterEp(linksList)
+		linksList = getLinks.getFilteredDownLinks()
 
-		for i in range(len(filteredList)):
-			print filteredList[i] + "\n"
-
-	# filter list, we only want to get episode that match our Quality and encoder
-	def filterEp(self, linksList):
-		# create list
-		filteredList = []
-		# iterate through the list
 		for i in range(len(linksList)):
-			links = linksList[i]
+			print linksList[i] + "\n"
 
-			# check if filter x265 encoder is set to True
-			if self.X265ENCODER == True:
-				# check if quality and encoder is in the links, add to the list
-				if self.QUALITY in links and "x265" in links:
-					filteredList.append(links)
-			else: 
-				# if encoder False, only check quality
-				if self.QUALITY in links:
-					filteredList.append(links)
-
-		return filteredList # return the list
-
-
-
-
-
+		print "\n---------------------------------------"
+		print "\n" + str(len(linksList)) + " episodes available for " + self.SERIES + " " + self.SEASON
+		print "\n---------------------------------------\n"
 
 if __name__ == '__main__':
 	SeriesDownloader().run()
